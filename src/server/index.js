2e507204ffdb62fc810ab454b6aee42dbe76c50f
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2020-11-24 22:02:51 +0000

    routes allocated

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,3 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const app = express();
@@ -9,19 +6,19 @@ const cors = require('cors');
 const { read } = require('fs');
 const { response } = require('express');
 const { ValidationError } = require('webpack');
+const route = require('./route');
 
 app.use(express.static('dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/', route);
 
 const route = require('./route');
 
 // designates what port the app will listen to for incoming requests
-app.listen(process.env.PORT || 8083, function () {
+module.exports = app.listen(process.env.PORT || 8083, function () {
     console.log('Example app listening on port 8083!')
 })
-
-module.exports = {app};
 
 
