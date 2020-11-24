99c837bdafffa52901f4a6d61b74c82d85958b4a
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2020-11-24 21:59:21 +0000

    afterAll to close the server

--- __test__/server.spec.js
+++ __test__/server.spec.js
@@ -1,11 +1,22 @@
-import { app } from '../src/server/route'
+const app = require ('../src/server/index')
 const supertest = require('supertest');
 const request = supertest(app); //provide a high-level abstraction for testing HTTP, while still allowing you to drop down to the lower-level API provided by superagent.
 
 describe('Testing post endpoint', () => {
-    it('testing /test endpoint', async done => {
-        const response = await request.get('/');
-        expect(response.body).toBeDefined();
-        done();
+    it('testing /test endpoint', async (done) => {
+        return request.get('/').then(data => {
+            expect(data).toBeDefined();
+            done();
+        })
+        .catch(err => {
+            console.log(error)
+            done();
+        })
     });
+})
+
+afterAll((done) => {
+    if (app) {
+        app.close(done);
+    }
 })
