99eab40dbab1403a5d136efb4ed466bedbb40f3d
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2020-11-24 22:04:44 +0000

    Update route.js

--- src/server/route.js
+++ src/server/route.js
@@ -7,29 +7,26 @@ let baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key=";
 let API_KEY = process.env.API_KEY;
 console.log(`API key: ${API_KEY}`)
 
+//display of UI
+app.get("/", function (req, res) {
+  res.status(200).sendFile("dist/index.html");
+});
 
-  //display of UI
-  app.get("/", function (req, res) {
-    res.status(200).sendFile("dist/index.html");
-  });
-
-  //POST request
-  app.post("/test", async (req, res) => {
-    urlEntry = req.body.url;
-    const response = await fetch(
-      `${baseUrl}${API_KEY}&of=json&txt&model=general&lang=en&url=${req.body.url}`
-    );
-    console.log("server response: ", response);
-    const data = await response.json();
-    console.log("server side: ", data);
-    const projectData = {
-      score_tag: data.score_tag,
-      confidence: data.confidence,
-      irony: data.irony,
-      subjectivity: data.subjectivity,
-    };
-    res.send(projectData);
-    console.log(projectData);
+//POST request
+app.post("/test", async (req, res) => {
+  urlEntry = req.body.url;
+  const response = await fetch(`${baseUrl}${API_KEY}&of=json&txt&model=general&lang=en&url=${req.body.url}`);
+  console.log("server response: ", response);
+  const data = await response.json();
+  console.log("server side: ", data);
+  const projectData = {
+     score_tag: data.score_tag,
+     confidence: data.confidence,
+     irony: data.irony,
+     subjectivity: data.subjectivity,
+   };
+   res.send(projectData);
+   console.log(projectData);
   });
 
 module.exports = {app};
