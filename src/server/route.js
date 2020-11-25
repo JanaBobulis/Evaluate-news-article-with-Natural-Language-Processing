4debf6574ecd6351cdcde0a3fa5f5c868af1150d
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2020-11-25 10:32:29 +0000

    dotenev

--- src/server/route.js
+++ src/server/route.js
@@ -1,24 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const fetch = require("node-fetch");
+const path = require('path');
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
 
-let baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key=";
+let baseUrl = 'https://api.meaningcloud.com/sentiment-2.1?key=';
 let API_KEY = process.env.API_KEY;
 console.log(`API key: ${API_KEY}`)
 
 //display of UI
-app.get("/", function (req, res) {
-  res.status(200).sendFile(path.resolve("dist/index.html"));
+app.get('/', function (req, res) {
+  res.status(200).sendFile(path.resolve('dist/index.html'));
 });
 
 //POST request
-app.post("/test", async (req, res) => {
+app.post('/test', async (req, res) => {
   urlEntry = req.body.url;
   const response = await fetch(`${baseUrl}${API_KEY}&of=json&txt&model=general&lang=en&url=${req.body.url}`);
-  console.log("server response: ", response);
+  console.log('server response: ', response);
   const data = await response.json();
-  console.log("server side: ", data);
+  console.log('server side: ', data);
   const projectData = {
      score_tag: data.score_tag,
      confidence: data.confidence,
